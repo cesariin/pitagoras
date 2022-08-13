b27add5fc78a5b888f60eb180b7dee725432778e
cesariin <105024647+cesariin@users.noreply.github.com>
2022-08-13 10:27:28 -0600

    Add files via upload

---
+++ pitagoras.py
@@ -0,0 +1,10 @@
+
+
+import math
+catetoa = float (input ('Ingresa el valor de cateto a: '))
+catetob = float (input ('Ingresa el valor de cateto b: '))
+hipotenusa = math.sqrt((catetoa*catetoa) + (catetob*catetob))
+print ('Valor de hipotenusa: ' + repr (hipotenusa))
+print ()
+
+
