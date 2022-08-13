cd8f83b48753a7ab4571f11b355f22196338d6aa
cesariin <105024647+cesariin@users.noreply.github.com>
2022-08-13 10:27:03 -0600

    Delete pitagoras.py

--- pitagoras.py
+++
@@ -1,6 +0,0 @@
-cateto_a = float (input ('Ingresa el valor de cateto a: '))
-cateto_b = float (input ('Ingresa el valor de cateto b: '))
-    hipotenusa=math.sqrt(cateto_a*cateto_a+cateto_b*cateto_b)
-    print ('Valor de hipotenusa: ' + repr (hipotenusa))
-print ()
-os.system ('pause')
